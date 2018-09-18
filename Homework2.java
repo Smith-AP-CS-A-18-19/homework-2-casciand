8e21e68af7990e115c014475a88a0d4cbd3f6e5c
casciand <casciand@berkeleyprep.org>
2018-09-18 14:11:03 -0400

    homework

--- Homework2.java
+++ Homework2.java
@@ -1,5 +1,5 @@
 /*
- * First and Last Names
+ * Andrew Cascio
  */
 
 import java.awt.Rectangle;
@@ -10,8 +10,13 @@ public class Homework2 {
 	 * It takes in one parameter: a String
 	 * Save the string as a private instance variable
 	 */
+	 	private String name;
+		private String one;
+		private String three;
 
-
+		public Homework2(String n) {
+				name = new String("Hello, World!");
+}
 
 	/* Write the method upperase
 	 * It does not take in any parameters
@@ -20,6 +25,11 @@ public class Homework2 {
 	 * uppercase letters and return the result
 	 * Hint: Search the String class for 'uppercase'
 	 */
+
+public String uppercase() {
+		one = name.toUpperCase();
+		return one;
+}
 
 
 	/* Write the method strip
@@ -30,25 +40,35 @@ public class Homework2 {
 	 * Hint: Search the String class for 'replace'
 	 */
 
+public String strip(String str) {
+		String two = new String("Good Morning, Dave!");
+		String three = two.replaceAll(" ,!", "");
+		return three;
+}
 
 	/* Fix the errors in the method problem4
 	 */
-	public Rectangle problems() {
-		Rectangle r1 = (5, 10, 15, 20);
-		double width = Rectangle(5, 10, 15, 20).getWidth();
-		Rectangle r2;
-		r2.translate(15, 25);
-		r3 = new Rectangle();
-		r3.translate("Far, far away");
-		return r3;
-	}
 
+	 public Rectangle problems() {
+		 Rectangle r1 = new Rectangle(5, 10, 15, 20);
+		 double width = r1.getWidth();
+		 Rectangle r2 = new Rectangle(5, 10, 15, 20);
+		 r2.translate(15, 25);
+		 Rectangle r3 = new Rectangle(5, 10, 15, 20);
+		 r3.translate(15, 25);
+		 return r3;
+}
 
 	/* Use Rectangle methods to calculate and return the perimeter of the
 	 * parameter Rectangle
 	 */
-	public double getPerimeter(Rectangle rect) {
 
+	public double getPerimeter(Rectangle rect) {
+			Rectangle recta = new Rectangle(5, 10, 20, 30);
+			double w = recta.getWidth();
+			double h = recta.getHeight();
+			double p = (2 * w) + (2 * h);
+			return p;
 	}
 
 }
